e2d4dd08931c7bfa1a9d10ffe72aebd82ec3e8e9
dzvag <dzvagins@gmail.com>
2026-02-03 15:03:58 +0000

    Changes for Vercel and Render

--- backend/src/main/java/com/luxurylife/backend/config/CorsConfig.java
+++ backend/src/main/java/com/luxurylife/backend/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
 
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "https://luxury-life-nine.vercel.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
